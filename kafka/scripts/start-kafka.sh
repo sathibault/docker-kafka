b36ae6ff15487a96064efda760c1a14bc8796a00
Scott Thibault <scott.thibault@multiscalehn.com>
2018-11-15 06:12:09 -0800

    Update to Kafka 1.1.1

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -14,22 +14,25 @@ if [ ! -z "$HELIOS_PORT_kafka" ]; then
     ADVERTISED_PORT=`echo $HELIOS_PORT_kafka | cut -d':' -f 2`
 fi
 
+#advertised.listeners=PLAINTEXT://your.host.name:9092
+ADVERTISE=PLAINTEXT:
+
 # Set the external host and port
 if [ ! -z "$ADVERTISED_HOST" ]; then
-    echo "advertised host: $ADVERTISED_HOST"
-    if grep -q "^advertised.host.name" $KAFKA_HOME/config/server.properties; then
-        sed -r -i "s/#(advertised.host.name)=(.*)/\1=$ADVERTISED_HOST/g" $KAFKA_HOME/config/server.properties
-    else
-        echo "advertised.host.name=$ADVERTISED_HOST" >> $KAFKA_HOME/config/server.properties
-    fi
+    ADVERTISE=$ADVERTISE//$ADVERTISED_HOST
+else
+    ADVERTISE=$ADVERTISE//
 fi
 if [ ! -z "$ADVERTISED_PORT" ]; then
-    echo "advertised port: $ADVERTISED_PORT"
-    if grep -q "^advertised.port" $KAFKA_HOME/config/server.properties; then
-        sed -r -i "s/#(advertised.port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
-    else
-        echo "advertised.port=$ADVERTISED_PORT" >> $KAFKA_HOME/config/server.properties
-    fi
+    ADVERTISE=$ADVERTISE:$ADVERTISED_PORT
+else
+    ADVERTISE=$ADVERTISE:9092
+fi
+echo "advertised listners: $ADVERTISE"
+if grep -q "^advertised.listners" $KAFKA_HOME/config/server.properties; then
+        sed -r -i "s/#(advertised.listners)=(.*)/\1=$ADVERTISE/g" $KAFKA_HOME/config/server.properties
+else
+    echo "\nadvertised.listners=$ADVERTISE" >> $KAFKA_HOME/config/server.properties
 fi
 
 # Set the zookeeper chroot
